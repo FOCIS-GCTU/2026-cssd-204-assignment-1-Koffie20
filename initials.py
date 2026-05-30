bfe3bc088d9e367bd935aca2ab48eca2bd363de1
Asare Gabriel Kofi <2425402613@live.gctu.edu.gh>
2026-05-30 22:00:47 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+# 
+# Name: Asare Gabriel Kofi
+# STUDENT ID:  2425402613
+# Email: 2425402613@live.gctu.edu.gh
+# Grader: Augustus
+# 
+# On my honor, Asare Gabriel Kofi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print stylized large letters representing my initials
+    print()
+    print("...AGK")
+    print()
+    print(".......AAAA...............GGGGGG...........KK........KK.....")
+    print("......AAAAAA............GGGGGGGGGG.........KK......KK.......")
+    print(".....AA....AA..........GG........GG........KK....KK.........")
+    print("....AA......AA.........GG..................KK..KK...........")
+    print("....AAAAAAAAAA.........GG....GGGGGG........KKKK.............")
+    print("....AAAAAAAAAA.........GG....GGGGGG........KKKK.............")
+    print("....AA......AA.........GG........GG........KK..KK...........")
+    print("....AA......AA.........GG........GG........KK....KK.........")
+    print("...AA........AA...**....GGGGGGGGGG....**...KK......KK.....**")
+    print("...AA........AA...**......GGGGGG......**...KK........KK...**")
+    print()
 
 
 main()
